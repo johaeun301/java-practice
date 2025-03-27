6ab6739f2422e3d3ddd4bc9931b63b83af8c328d
johaeun301 <haeunj125@gmail.com>
2025-03-27 14:35:49 +0900

    Add files via upload

---
+++ 2024011868/src/java1.java
@@ -0,0 +1,8 @@
+public class java1 {
+    public static void main(String[] args) {
+        String name = "조하은";
+        int age = 18;
+
+        System.out.printf(name + "님은" + age + "살 입니다.");
+    }
+}
---
+++ 2024011868/src/java2.java
@@ -0,0 +1,9 @@
+public class java2 {
+    public static void main(String[] args) {
+        double round = 10.0;
+        final double PI = 3.14159265358979;
+        double result = round * round * PI;
+
+        System.out.println(result);
+    }
+}
